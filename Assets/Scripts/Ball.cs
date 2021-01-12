231856864211e6dd8e37a086d9fdfa4015540026
oceanncs <59238106+oceanncs@users.noreply.github.com>
2021-01-12 20:03:21 +0000

    DodgeBall3D - Game Base
    Player catches the ball and it reflects on walls

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public  class Ball : MonoBehaviour
+{
+    private readonly Vector3 offset = new Vector3(1f, 0f, 0f);
+    
+   
+    
+    private bool isGrabbed;
+
+    [SerializeField] private LayerMask wallLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrabbed = false;
+        
+
+    }
+
+
+
+    void Update()
+    {
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray,out hit, 20f * Time.deltaTime, wallLayer))
+        {
+            Vector3 newDirection = Vector3.Reflect(ray.direction, hit.normal);
+            
+            
+            float rotation = 90f - Mathf.Atan2(newDirection.z, newDirection.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0f, rotation, 0f);
+            // transform.forward = newDirection;
+
+        }
+    }
+
+   public void GrabBall(Transform playerThatGrabs)
+    {
+        if (!isGrabbed)
+        {
+            isGrabbed = true;
+            transform.SetParent(playerThatGrabs.transform);
+            transform.localPosition = offset;
+            transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    public void ThrowBall()
+    {
+        transform.SetParent(null);
+        StartCoroutine(BallThrow());
+
+    }
+
+
+    IEnumerator BallThrow()
+    {
+        isGrabbed = false;
+        
+
+        while (!isGrabbed)
+        {
+            transform.position += transform.forward * (20f * Time.deltaTime);
+            yield return null;
+        }
+    }
+    
+    
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed;
+
+    [SerializeField] private Camera sceneCamera;
+    // Start is called before the first frame update
+
+
+    private bool canCatch;
+    [SerializeField] Ball ball;
+    [SerializeField] private float angle;
+    
+    
+    private Rigidbody playerRb;
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        if (x != 0 || z != 0)
+        {
+            playerRb.position += Vector3.forward * (z * Time.deltaTime * movementSpeed);
+            playerRb.position += Vector3.right * (x * Time.deltaTime * movementSpeed);
+        }
+        else playerRb.velocity = Vector3.zero;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        MovementAndOrientationUpdate();
+        
+        if(Input.GetMouseButtonUp(0)) TryThrow();
+    }
+
+    
+
+    void MovementAndOrientationUpdate()
+    {
+
+        Vector3 mousePoint = sceneCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,sceneCamera.transform.position.y));
+        mousePoint.y *= 0f;
+        
+        transform.LookAt(mousePoint);
+    }
+    
+    void TryThrow()
+    {
+       
+            Ball ball = GetComponentInChildren<Ball>();
+            ball?.ThrowBall();
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == ball.transform) canCatch = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == ball.transform) canCatch = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!Input.GetMouseButtonDown(0)) return; 
+        
+        
+        float angleToBall = Vector3.Angle(transform.forward, ball.transform.position-transform.position);
+
+        if (canCatch && angleToBall >= -angle / 2 && angleToBall <= angle / 2)
+        {
+            Debug.Log("CATCHED" + canCatch);
+            ball.GrabBall(this.transform);
+            canCatch = false;
+        }
+          
+    }
+}
